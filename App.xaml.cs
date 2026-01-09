861768306b676549ae10d420b466d3010091c49a
xiaoxiao <xiaojundi_xx@163.com>
2026-01-10 01:05:17 +0800

    删除所有代码

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,19 +11,4 @@ namespace HexaFlow;
 /// </summary>
 public partial class App : Application
 {
-    private static ConfigService _configService;
-
-    /// <summary>
-    /// 获取配置服务实例
-    /// </summary>
-    public static ConfigService ConfigService => _configService;
-
-    protected override async void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-
-        // 初始化配置服务
-        _configService = new ConfigService();
-        await _configService.LoadConfigurationsAsync();
-    }
 }
